0b797152ea524ec0b8a7820099b9d91f91784c99
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-03-30 00:26:10 +0530

    Update mypipelineparamwithdockersonartrivy.groovy
    Update mypipelineparamwithdockersonartrivy.groovy

--- vars/mypipelineparamwithdockersonartrivy.groovy
+++ vars/mypipelineparamwithdockersonartrivy.groovy
@@ -84,14 +84,18 @@ def call(Map config = [:]) {
     }
 }
 
-            // ✅ OPTIONAL: Quality Gate (recommended)
-            stage('Quality Gate') {
-                steps {
-                    timeout(time: 5, unit: 'MINUTES') {
-                        waitForQualityGate abortPipeline: true
-                    }
+       stage('Quality Gate') {
+    steps {
+        timeout(time: 10, unit: 'MINUTES') {
+            script {
+                def qg = waitForQualityGate()
+                if (qg.status != 'OK') {
+                    echo "Quality Gate failed: ${qg.status}"
                 }
             }
+        }
+    }
+}
 
             stage('Archive Artifacts') {
                 steps {
