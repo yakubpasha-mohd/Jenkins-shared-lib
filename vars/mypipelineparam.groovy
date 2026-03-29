3c4992b373e249fb0fc276d202e5d33f87aa1053
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-03-29 18:05:47 +0530

    Update mypipelineparam.groovy

--- vars/mypipelineparam.groovy
+++ vars/mypipelineparam.groovy
@@ -1,30 +1,25 @@
 def call(Map config = [:]) {
 
-    
     def appName = config.appName ?: "java-app"
-    def repoUrl = config.repoUrl ?: ""
-    def branch  = config.branch ?: "main"
-    def envName = config.environment ?: "dev"
 
     pipeline {
         agent any
 
-        tools {
-            maven 'Maven3'
-            jdk 'openjdk-17'
+        parameters {
+            string(name: 'REPO_URL', defaultValue: '', description: 'Git Repo URL')
+            string(name: 'BRANCH', defaultValue: 'main', description: 'Git Branch')
+            choice(name: 'ENV', choices: ['dev', 'qa', 'prod'], description: 'Environment')
         }
 
         environment {
             APP_NAME = "${appName}"
-            DEPLOY_ENV = "${envName}"
         }
 
         stages {
 
             stage('Checkout') {
                 steps {
-                    echo "Checking out from ${repoUrl} (branch: ${branch})"
-                    git url: repoUrl, branch: branch
+                    git url: params.REPO_URL, branch: params.BRANCH
                 }
             }
 
@@ -34,43 +29,10 @@ def call(Map config = [:]) {
                 }
             }
 
-            stage('Test') {
-                steps {
-                    sh 'mvn test'
-                }
-            }
-
-            stage('Archive Artifacts') {
-                steps {
-                    archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
-                }
-            }
-
             stage('Deploy') {
                 steps {
-                    echo "Deploying ${APP_NAME} to ${DEPLOY_ENV}"
-                    
-                    script {
-                        if (envName == 'dev') {
-                            echo "Deploying to DEV environment"
-                        } else if (envName == 'qa') {
-                            echo "Deploying to QA environment"
-                        } else if (envName == 'prod') {
-                            echo "🚀 Deploying to PROD environment"
-                        } else {
-                            error "Invalid environment: ${envName}"
-                        }
-                    }
+                    echo "Deploying ${APP_NAME} to ${params.ENV}"
                 }
-            }
-        }
-
-        post {
-            success {
-                echo "Pipeline Success ✅ (${DEPLOY_ENV})"
-            }
-            failure {
-                echo "Pipeline Failed ❌ (${DEPLOY_ENV})"
             }
         }
     }
