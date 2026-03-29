78c465014baec077b7fb9ea0974ad801434eb664
yakubpasha-mohd <43091747+yakubpasha-mohd@users.noreply.github.com>
2026-03-29 14:26:54 +0530

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,58 @@
+def call(Map config = [:]) {
+
+    pipeline {
+        agent any
+
+        tools {
+            maven 'Maven3'
+            jdk 'JDK11'
+        }
+
+        environment {
+            APP_NAME = config.appName ?: "java-app"
+        }
+
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    git url: config.repoUrl, branch: 'main'
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh 'mvn clean package'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+            }
+
+            stage('Archive Artifacts') {
+                steps {
+                    archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
+                }
+            }
+
+            stage('Deploy (Local)') {
+                steps {
+                    echo "Deploying ${APP_NAME}"
+                    sh 'ls -l target/'
+                }
+            }
+        }
+
+        post {
+            success {
+                echo 'Pipeline Success ✅'
+            }
+            failure {
+                echo 'Pipeline Failed ❌'
+            }
+        }
+    }
+}
